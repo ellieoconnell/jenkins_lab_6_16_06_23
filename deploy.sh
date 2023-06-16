b2285a9fbcf6a8f72a783cf88772b976236b9c40
ellieoconnell <116805774+ellieoconnell@users.noreply.github.com>
2023-06-16 15:21:19 +0100

    //

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ docker run -d \
     --name nginx \
     -p 80:80 \
     --network trio-task-network \
-    --mount type=bind,source$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
+    --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
     nginx:latest
 
 echo 
